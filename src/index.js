81d4c6c4478c19b7ec4304147e82cff7486cd955
Jamie Phan <9488815+jamiephan@users.noreply.github.com>
2021-09-06 21:36:22 +0100

    Removed /ratelimit testing path

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 require("dotenv").config()
 
 const express = require("express")
-const fetch = require("node-fetch")
 
 const app = express()
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
-const respError = require("./Model/Responses/Error")
 
 app.use(express.static('public'))
 
@@ -16,16 +13,7 @@ app.use("/list", require("./routes/api/list"))
 app.use("/templates", require("./routes/api/templates"))
 app.use("/build", require("./routes/api/build"))
 
-app.get("/ratelimit", async (req, res) => {
-  try {
-    const response = await fetch(`https://${host}/rate_limit`)
-    const json = await response.json()
-    res.json(json)
-  } catch (e) {
-    res.status(500).json(respError("Github API Error"))
-  }
-})
-
+// Redirect everything back to "/", the webapp interface
 app.all("*", (req, res) => {
   res.redirect("/")
 })
