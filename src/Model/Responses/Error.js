61bcfbe146f4e13942f0ad583929f58eeaa7b8cb
Jamie Phan <9488815+jamiephan@users.noreply.github.com>
2021-09-06 04:24:15 +0100

    Added JSDOC for error response

--- src/Model/Responses/Error.js
+++ src/Model/Responses/Error.js
@@ -1,3 +1,14 @@
+/**
+ * @typedef {Object} ErrorResponse
+ * @property {error} error true
+ * @property {string} message The Error message provided
+ */
+
+/**
+ * Generate Error Message Response JSON
+ * @param {String} message The error message
+ * @returns {ErrorResponse}
+ */
 module.exports = (message) => {
   return {
     error: true,
