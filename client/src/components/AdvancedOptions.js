1ca1173c0b4f2979eb0772a5d0187086900c2097
Jamie Phan <9488815+jamiephan@users.noreply.github.com>
2021-09-10 07:42:53 +0100

    Updated AdvancedOptions

--- client/src/components/AdvancedOptions.js
+++ client/src/components/AdvancedOptions.js
@@ -7,6 +7,7 @@ export default function AdvancedOptions(props) {
   const [isLoadingOptions, setIsLoadingOptions] = useState(true)
   const [isShowingAdvancedOptionAlertBox, setIsShowingAdvancedOptionAlertBox] = useState(true)
   const [options, setOptions] = useState([])
+  const [changes, setChanges] = useState([])
 
   useEffect(() => {
     (async () => {
@@ -32,60 +33,67 @@ export default function AdvancedOptions(props) {
 
   useEffect(() => {
     if (!isLoadingOptions) {
-      props.set(options.map(s => s.libraries)
+      const variables = options.map(s => s.libraries)
         .reduce((p, n) => p.concat(n))
         .map(x => x.options)
         .reduce((p, n) => p.concat(n))
-        .filter(x => x.value !== x.default)
-        .map(x => x.name))
+
+      const tempChanges = []
+
+      for (const variable of variables) {
+        if (variable.value !== variable.default) {
+          console.log(variable.name)
+          tempChanges.push(variable)
+        }
+      }
+
+      setChanges(tempChanges)
+      props.set(tempChanges.map(v => v.name))
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [options])
 
   return (
     <>
-      <br />
-      {isShowingAdvancedOptionAlertBox ?
+      <div style={{ margin: "20px 0px" }}>
+        {isShowingAdvancedOptionAlertBox ?
 
-        <Alert variant="warning" dismissible="true" onClose={() => setIsShowingAdvancedOptionAlertBox(false)}>
-          <h5>What is this?</h5>
-          <ul style={{ marginBottom: "0" }}>
-            <li>This section allows you to modify each non-constant variables in the game (currently only supports <code>Boolean</code> type).</li>
-            <li>The script will be injected into the <code>MapScript.galaxy</code> after all libraries have been initialized.</li>
-            <li>Each of the sections belows represent a single library from the game. </li>
-            <li>The default values were gathered via the <code>InitVariables()</code> function from their respective library.</li>
-            <hr />
-          </ul>
-          <h5>Notes:</h5>
-          <ul style={{ marginBottom: "0" }}>
-            <li>The settings on this page <b>will not</b> be saved on your browser, refreshing will reset to default.</li>
-            <li>Some variables depends on certain variables to function properly or some might not have any effect. You will need to test or study the trigger code manually.</li>
-            <li>For <code><b>Maps</b></code> and <code><b>Brawl</b></code> libraries, <b>Make sure your map have the required mods, or the map will crash when launched.</b></li>
-            <hr />
-          </ul>
-          <h5>Useful Options:</h5>
-          <ul style={{ marginBottom: "0" }}>
-            <li>Core <ArrowRight /> Heroes <ArrowRight /> <code>libCore_gv_dEBUGDebuggingEnabled</code>: Toggle Debug Mode</li>
-            <li>Core <ArrowRight /> Heroes <ArrowRight /> <code>libCore_gv_dEBUGPingLimitEnabled </code>: Toggle Ping Limit</li>
-            <li>Core <ArrowRight /> Heroes <ArrowRight /> <code>libCore_gv_sYSMinionSpawningOn</code>: Toggle Minion Waves</li>
-            <li>Core <ArrowRight /> Game <ArrowRight /> <code>libGame_gv_afk_UseAFKSystem</code>: Toggle AFK detection</li>
-            <li>Core <ArrowRight /> Support <ArrowRight /> <code>libSprt_gv_dEBUGNoRegen</code>: Toggle HP / Energy Regen</li>
-            <li>Core <ArrowRight /> AI <ArrowRight /> <code>libAIAI_gv_heroAIDisplayAIStatus</code>: Toggle showing AI status</li>
-          </ul>
-        </Alert> :
-        <><Button variant="warning" onClick={() => { setIsShowingAdvancedOptionAlertBox(true) }}>Show Advanced Option Description</Button><br /><br /></>
-      }
+          <Alert variant="warning" dismissible="true" onClose={() => setIsShowingAdvancedOptionAlertBox(false)}>
+            <h5>What is this?</h5>
+            <ul style={{ marginBottom: "0" }}>
+              <li>This section allows you to modify each non-constant variables in the game (currently only supports <code>Boolean</code> type).</li>
+              <li>The script will be injected into the <code>MapScript.galaxy</code> after all libraries have been initialized.</li>
+              <li>Each of the sections belows represent a single library from the game. </li>
+              <li>The default values were gathered via the <code>InitVariables()</code> function from their respective library.</li>
+              <hr />
+            </ul>
+            <h5>Notes:</h5>
+            <ul style={{ marginBottom: "0" }}>
+              <li>The settings on this page <b>will not</b> be saved on your browser, refreshing will reset to default.</li>
+              <li>Some variables depends on certain variables to function properly or some might not have any effect. You will need to test or study the trigger code manually.</li>
+              <li>For <code><b>Maps</b></code> and <code><b>Brawl</b></code> libraries, <b>Make sure your map have the required mods, or the map will crash when launched.</b></li>
+              <hr />
+            </ul>
+            <h5>Useful Options:</h5>
+            <ul style={{ marginBottom: "0" }}>
+              <li>Core <ArrowRight /> Heroes <ArrowRight /> <code>libCore_gv_dEBUGDebuggingEnabled</code>: Toggle Debug Mode</li>
+              <li>Core <ArrowRight /> Heroes <ArrowRight /> <code>libCore_gv_dEBUGPingLimitEnabled </code>: Toggle Ping Limit</li>
+              <li>Core <ArrowRight /> Heroes <ArrowRight /> <code>libCore_gv_sYSMinionSpawningOn</code>: Toggle Minion Waves</li>
+              <li>Core <ArrowRight /> Game <ArrowRight /> <code>libGame_gv_afk_UseAFKSystem</code>: Toggle AFK detection</li>
+              <li>Core <ArrowRight /> Support <ArrowRight /> <code>libSprt_gv_dEBUGNoRegen</code>: Toggle HP / Energy Regen</li>
+              <li>Core <ArrowRight /> AI <ArrowRight /> <code>libAIAI_gv_heroAIDisplayAIStatus</code>: Toggle showing AI status</li>
+            </ul>
+          </Alert> :
+          <><Button variant="warning" onClick={() => { setIsShowingAdvancedOptionAlertBox(true) }}>Show Advanced Option Description</Button></>
+        }
+      </div>
       <h4>Changes:</h4>
-      <ul>
-        {
-          isLoadingOptions ? <></> :
-            options
-              .map(s => s.libraries)
-              .reduce((p, n) => p.concat(n))
-              .map(x => x.options)
-              .reduce((p, n) => p.concat(n))
-              .map((x, i) => x.value !== x.default ?
-                <li key={i}>
+      {
+        (changes.length > 0) && (!isLoadingOptions) ?
+          <>
+            <ul>
+              {changes.map(x =>
+                <li key={x.name}>
                   <code>{x.name} = </code>
                   <code style={{ color: x.default ? "green" : "red" }}>{String(x.default)}</code>
                   {" "}
@@ -95,25 +103,26 @@ export default function AdvancedOptions(props) {
                     <b>{String(x.value)}</b>
                   </code>
                 </li>
-                : null)
-        }
-      </ul>
-      <br />
-      <Button variant="danger" onClick={() => {
-        if (window.confirm("You sure want to reset the options to default?")) {
-          const ol = JSON.parse(JSON.stringify(options))
-          ol.forEach(s => {
-            s.libraries.forEach(d => {
-              d.options.forEach(o => {
-                o.value = o.default
-              })
-            })
-          })
-          setOptions(ol)
-        }
-      }}
-      >Reset ALL to default</Button>
+              )}
+            </ul>
 
+            <Button variant="danger" onClick={() => {
+              if (window.confirm("You sure want to reset the options to default?")) {
+                const ol = JSON.parse(JSON.stringify(options))
+                ol.forEach(s => {
+                  s.libraries.forEach(d => {
+                    d.options.forEach(o => {
+                      o.value = o.default
+                    })
+                  })
+                })
+                setOptions(ol)
+              }
+            }}
+            >Reset ALL to default</Button>
+
+          </> : <ul><li><i>No changed were made.</i></li></ul>
+      }
       {
         isLoadingOptions ? <></> :
           options.map((s, i) =>
